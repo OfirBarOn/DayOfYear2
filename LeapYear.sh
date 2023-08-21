ee97fa47fe31c40bdfbb2ff550b8c43342b06482
OfirBarOn <123425529+OfirBarOn@users.noreply.github.com>
2023-08-21 17:48:02 +0300

    Update LeapYear.sh

--- LeapYear.sh
+++ LeapYear.sh
@@ -2,30 +2,34 @@
 
 # Check if leap year
 
-NumberValidation="^[a-zA-Z]*$"
+function LeapYear(){
+	NumberValidation="^[a-zA-Z]*$"
+	
+	read -p "Enter a year: " year
+	
+	if [[ $year =~ $NumberValidation ]]; then
+		echo "Wrong input"
+		exit
+	#else
+	#	echo number
+	fi
+	
+	if [ $year -lt 1582 ]; then
+		echo "Not within the Gregorian calendar period"
+		exit
+	#else
+	#	echo success
+	fi
+	
+	if [ $(($year%4)) != 0 ]; then
+		echo "Common year"
+	elif [ $(($year%100)) != 0 ]; then
+		echo "Leap year"
+	elif [ $(($year%400)) != 0 ]; then
+		echo "Common year"
+	else
+		echo "Leap year"
+	fi
+}
 
-read -p "Enter a year: " year
-
-if [[ $year =~ $NumberValidation ]]; then
-	echo "Wrong input"
-	exit
-#else
-#	echo number
-fi
-
-if [ $year -lt 1582 ]; then
-	echo "Not within the Gregorian calendar period"
-	exit
-#else
-#	echo success
-fi
-
-if [ $(($year%4)) != 0 ]; then
-	echo "Common year"
-elif [ $(($year%100)) != 0 ]; then
-	echo "Leap year"
-elif [ $(($year%400)) != 0 ]; then
-	echo "Common year"
-else
-	echo "Leap year"
-fi
+LeapYear()
